dd816a3172656e09f6aec0188c2f215e873eb660
nehidnytsya <janadum2609@gmail.com>
2023-05-17 23:18:34 +0200

    in process 2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+from flask import abort, redirect, url_for, make_response
 app = Flask(__name__)
 @app.route('/')
 def index():
@@ -6,3 +7,17 @@ def index():
 @app.route('/about')
 def about():
  return app.send_static_file('about.html')
+@app.route('/error_denied')
+def error_denied():
+ abort(401)
+@app.route('/error_internal')
+def error_internal():
+    return render_template('template.html', name='ERROR 505'), 505
+@app.route('/error_not_found')
+def error_not_found():
+    response = make_response(render_template('template.html', name='ERROR 404'), 404)
+    response.headers['X-Something'] = 'A value'
+    return response
+@app.errorhandler(404)
+def not_found_error(error):
+ return render_template('404.html'), 404
